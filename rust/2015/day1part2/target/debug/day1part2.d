a51457f0dea96bc164bdcad7ecc25ebe95ee9e0a
jacksmithinsulander <666.jack.smith@protonmail.com>
2023-06-25 22:04:59 +0200

    reorganized the rust structure for a bit

---
+++ rust/2015/day1part2/target/debug/day1part2.d
@@ -0,0 +1,1 @@
+/home/ted/prog/aoc/rust/2015/day1part2/target/debug/day1part2: /home/ted/prog/aoc/rust/2015/day1part2/src/main.rs
---
+++ rust/2015/day1part2/target/debug/deps/day1part2-c429ee575f94ce4a.d
@@ -0,0 +1,5 @@
+/home/ted/prog/aoc/rust/2015/day1part2/target/debug/deps/day1part2-c429ee575f94ce4a: src/main.rs
+
+/home/ted/prog/aoc/rust/2015/day1part2/target/debug/deps/day1part2-c429ee575f94ce4a.d: src/main.rs
+
+src/main.rs:
