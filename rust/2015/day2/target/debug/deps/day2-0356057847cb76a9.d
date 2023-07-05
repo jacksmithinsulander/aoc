c3989bf2630366c74cf6869a7da02fc36e525603
Jack Smith Insulander <106760392+jacksmithinsulander@users.noreply.github.com>
2023-07-05 14:22:39 +0200

    Delete rust/2015/day2/target directory

--- rust/2015/day2/target/debug/deps/day2-0356057847cb76a9.d
+++
@@ -1,5 +0,0 @@
-/home/ted/prog/aoc/rust/2015/day2/target/debug/deps/day2-0356057847cb76a9: src/main.rs
-
-/home/ted/prog/aoc/rust/2015/day2/target/debug/deps/day2-0356057847cb76a9.d: src/main.rs
-
-src/main.rs:
