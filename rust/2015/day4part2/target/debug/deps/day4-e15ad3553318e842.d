dd3167e085df887f9759378bec603a8e44eafafa
Jack Smith Insulander <106760392+jacksmithinsulander@users.noreply.github.com>
2023-07-05 14:24:18 +0200

    Delete rust/2015/day4part2/target directory

--- rust/2015/day4part2/target/debug/deps/day4-e15ad3553318e842.d
+++
@@ -1,5 +0,0 @@
-/home/ted/prog/aoc/rust/2015/day4/target/debug/deps/day4-e15ad3553318e842: src/main.rs
-
-/home/ted/prog/aoc/rust/2015/day4/target/debug/deps/day4-e15ad3553318e842.d: src/main.rs
-
-src/main.rs:
