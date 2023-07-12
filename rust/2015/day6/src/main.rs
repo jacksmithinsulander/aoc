925ac8b7643e4a2ef753043b665eec857da4f8e1
jacksmithinsulander <666.jack.smith@protonmail.com>
2023-07-12 14:14:53 +0200

    kinda bited the whole thing from a youtube vid but hey at least you learn  ¯\_(ツ)_/¯

--- rust/2015/day6/src/main.rs
+++ rust/2015/day6/src/main.rs
@@ -1,39 +1,78 @@
 use regex::Regex;
 
-struct Instructor {
+struct Instruction {
     instruction: String,
     start: Vec<usize>,
     end: Vec<usize>,
 }
 
-fn main() {
-    //let data = include_str!("./inmput.txt");
-
-    let data = "turn off 854,56 through 965,591";
-    let parser = Regex::new(
-        r"(?P<inst>turn off|turn on|toggle) (?P<start>\d+,\d+) through (?P<end>\d+,\d+)",
-    )
-    .unwrap();
-    let captures = parser.captures(data).ok_or("no matches").unwrap();
-    let instruction = captures.name("inst").map_or("", |m| m.as_str()).to_string();
-    let start: Vec<usize> = captures
-        .name("start")
-        .map_or("", |m| m.as_str())
-        .split(',')
-        .map(|s| s.parse().expect("Parse error, {s} not an int"))
-        .collect();
-    let end: Vec<usize> = captures
-        .name("end")
-        .map_or("", |m| m.as_str())
-        .split(',')
-        .map(|s| s.parse().expect("Parse error, {s} not an int"))
-        .collect();
-
-    let i: Instruction {
-        instruction: instruction,
-        start: start,
-        end: end,
+impl From<&str> for Instruction {
+    fn from(s: &str) -> Instruction {
+        let parser = Regex::new(
+            r"(?P<inst>turn off|turn on|toggle) (?P<start>\d+,\d+) through (?P<end>\d+,\d+)",
+        )
+        .unwrap();
+        let captures = parser.captures(s).ok_or("no matches").unwrap();
+        let instruction = captures.name("inst").map_or("", |m| m.as_str()).to_string();
+        let start: Vec<usize> = captures
+            .name("start")
+            .map_or("", |m| m.as_str())
+            .split(',')
+            .map(|s| s.parse().expect("Parse error, {s} not an int"))
+            .collect();
+        let end: Vec<usize> = captures
+            .name("end")
+            .map_or("", |m| m.as_str())
+            .split(',')
+            .map(|s| s.parse().expect("Parse error, {s} not an int"))
+            .collect();
+        Instruction {
+            instruction: instruction,
+            start: start,
+            end: end,
+        }
     }
-    println!("Part 1: {:?}", start);
-    println!("Part 2: {:?}", end)
+}
+
+#[derive(Debug)]
+struct Grid {
+    grid: [[u8; 1000]; 1000],
+}
+
+impl Grid {
+    fn new() -> Self {
+        Grid { grid: [[0u8; 1000]; 1000]}
+    }
+    fn process_instruction(&mut self, inst: Instruction) {
+        for r in inst.start[0]..inst.end[0]+1 {
+            for c in inst.start[1]..inst.end[1]+1 {
+                match inst.instruction.as_str() {
+                    "turn on" => self.grid[r][c] = 1,
+                    "turn off" => self.grid[r][c] = 0,
+                    "toggle" => self.grid[r][c] = (self.grid[r][c]) % 2,
+                    _ => panic!("Unexpected Instruction {}", inst.instruction),
+                }
+            } 
+        }
+    }
+    fn sum_lights(&self) -> usize {
+        self.grid
+            .iter()
+            .fold(0, |acc, row| {
+                acc + row.iter().fold(0, |inner_acc, cell| inner_acc + *cell as usize) as usize
+            })
+    }
+}
+
+fn main() {
+    let data = include_str!("./input.txt");
+    let mut g = Grid::new();
+
+    for line in data.lines() {
+        let inst: Instruction = line.into();
+        g.process_instruction(inst)
+    }
+
+    println!("Part 1: {}", g.sum_lights());
+    println!("Part 2: {:?}", "");
 }
